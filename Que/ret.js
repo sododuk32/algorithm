12519afed0a295453ac5a4635783e1d40753b300
dy kwak <sododuk63@github.com>
2025-02-05 18:08:57 +0900

    Create ret.js

---
+++ Que/ret.js
@@ -0,0 +1,52 @@
+class Queue {
+constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+    }
+
+    enqueue(value){
+        let nodes = new Node(value);
+
+        if(!this.first) {
+            this.first = nodes;
+            this.last = nodes;
+        }
+        else {
+            this.last.next = nodes;
+            this.last = nodes;
+        }
+        this.size++;
+    }
+    dequeue(){
+        
+        if(!this.first) return undefined;
+        
+        const output = this.first;
+        this.first =  this.first.next;
+
+        if(!this.first) this.last = null;
+            
+    
+        return output.value;
+    }
+
+}
+
+class Node{
+constructor(value)
+{
+    this.value = value;
+    this.next = null;
+}
+
+}
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.dequeue()); // 10
+console.log(queue.dequeue()); // 20
+console.log(queue.dequeue()); // 30
+console.log(queue.dequeue());
